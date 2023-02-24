f005d7e967cd309922ba07e607e6e87ebdae87a0
smtmfft <smtm@taiko.xyz>
2023-02-24 09:22:00 +0000

    update to new pi

--- prover/src/circuits.rs
+++ prover/src/circuits.rs
@@ -6,46 +6,46 @@ use zkevm_circuits::copy_circuit::CopyCircuit;
 use zkevm_circuits::evm_circuit::EvmCircuit;
 use zkevm_circuits::exp_circuit::ExpCircuit;
 use zkevm_circuits::keccak_circuit::keccak_packed_multi::KeccakCircuit;
-use zkevm_circuits::pi_circuit::PiCircuit;
-use zkevm_circuits::pi_circuit::PiTestCircuit;
+use zkevm_circuits::pi_circuit2::PiCircuit;
+use zkevm_circuits::pi_circuit2::PiTestCircuit;
 use zkevm_circuits::state_circuit::StateCircuit;
 use zkevm_circuits::super_circuit::SuperCircuit;
 use zkevm_circuits::tx_circuit::TxCircuit;
 use zkevm_circuits::util::SubCircuit;
 
 /// Returns a instance of the `SuperCircuit`.
-pub fn gen_super_circuit<
-    const MAX_TXS: usize,
-    const MAX_CALLDATA: usize,
-    const MAX_RWS: usize,
-    RNG: Rng,
->(
-    witness: &CircuitWitness,
-    mut _rng: RNG,
-) -> Result<SuperCircuit<Fr, MAX_TXS, MAX_CALLDATA, MAX_RWS>, String> {
-    let block = witness.evm_witness();
+// pub fn gen_super_circuit<
+//     const MAX_TXS: usize,
+//     const MAX_CALLDATA: usize,
+//     const MAX_RWS: usize,
+//     RNG: Rng,
+// >(
+//     witness: &CircuitWitness,
+//     mut _rng: RNG,
+// ) -> Result<SuperCircuit<Fr, MAX_TXS, MAX_CALLDATA, MAX_RWS>, String> {
+//     let block = witness.evm_witness();
 
-    let evm_circuit = EvmCircuit::new_from_block(&block);
-    let state_circuit = StateCircuit::new_from_block(&block);
-    let tx_circuit = TxCircuit::new_from_block(&block);
-    let pi_circuit = PiCircuit::new_from_block(&block);
-    let bytecode_circuit = BytecodeCircuit::new_from_block(&block);
-    let copy_circuit = CopyCircuit::new_from_block(&block);
-    let exp_circuit = ExpCircuit::new_from_block(&block);
-    let keccak_circuit = KeccakCircuit::new_from_block(&block);
-    let circuit = SuperCircuit::<_, MAX_TXS, MAX_CALLDATA, MAX_RWS> {
-        evm_circuit,
-        state_circuit,
-        tx_circuit,
-        pi_circuit,
-        bytecode_circuit,
-        copy_circuit,
-        exp_circuit,
-        keccak_circuit,
-    };
+//     let evm_circuit = EvmCircuit::new_from_block(&block);
+//     let state_circuit = StateCircuit::new_from_block(&block);
+//     let tx_circuit = TxCircuit::new_from_block(&block);
+//     let pi_circuit = PiCircuit::new_from_block(&block);
+//     let bytecode_circuit = BytecodeCircuit::new_from_block(&block);
+//     let copy_circuit = CopyCircuit::new_from_block(&block);
+//     let exp_circuit = ExpCircuit::new_from_block(&block);
+//     let keccak_circuit = KeccakCircuit::new_from_block(&block);
+//     let circuit = SuperCircuit::<_, MAX_TXS, MAX_CALLDATA, MAX_RWS> {
+//         evm_circuit,
+//         state_circuit,
+//         tx_circuit,
+//         pi_circuit,
+//         bytecode_circuit,
+//         copy_circuit,
+//         exp_circuit,
+//         keccak_circuit,
+//     };
 
-    Ok(circuit)
-}
+//     Ok(circuit)
+// }
 
 /// Returns a instance of the `PiTestCircuit`.
 pub fn gen_pi_circuit<
