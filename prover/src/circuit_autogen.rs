4dcc62b9abd8dd5b03f1c0e34793e0218ae30fca
smtmfft <smtm@taiko.xyz>
2023-09-20 10:24:41 +0000

    enlarge gas limit to 8200000

--- prover/src/circuit_autogen.rs
+++ prover/src/circuit_autogen.rs
@@ -2,10 +2,10 @@
 macro_rules! match_circuit_params {
     ($gas_used:expr, $on_match:expr, $on_error:expr) => {
         match $gas_used {
-            0..=8000000 => {
+            0..=8200000 => {
                 const CIRCUIT_CONFIG: CircuitConfig = CircuitConfig {
-                    block_gas_limit: 800000,
-                    max_txs: 14,
+                    block_gas_limit: 820000,
+                    max_txs: 80,
                     max_calldata: 69750,
                     max_bytecode: 139500,
                     max_rws: 3161966,
