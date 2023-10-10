3d00de0cb2907a14137442f15ab4b6dcbcebb085
smtmfft <smtm@taiko.xyz>
2023-10-10 17:20:04 +0800

    update circuit conf
    Signed-off-by: qcloud <ubuntu@localhost.localdomain>

--- prover/src/circuit_autogen.rs
+++ prover/src/circuit_autogen.rs
@@ -2,7 +2,7 @@
 macro_rules! match_circuit_params {
     ($gas_used:expr, $on_match:expr, $on_error:expr) => {
         match $gas_used {
-            0..=10000 => {
+            0..=100 => {
                 const CIRCUIT_CONFIG: CircuitConfig = CircuitConfig {
                     block_gas_limit: 820000,
                     max_txs: 80,
@@ -13,23 +13,23 @@ macro_rules! match_circuit_params {
                     max_exp_steps: 27900,
                     min_k: 19,
                     pad_to: 0,
-                    min_k_aggregation: 22,
+                    min_k_aggregation: 26,
                     keccak_padding: 500000,
                 };
                 $on_match
             }
-            10001..=8000000 => {
+            101..=8000000 => {
                 const CIRCUIT_CONFIG: CircuitConfig = CircuitConfig {
                     block_gas_limit: 800000,
                     max_txs: 30,
                     max_calldata: 69750,
                     max_bytecode: 139500,
-                    max_rws: 500000,
-                    max_copy_rows: 500000,
+                    max_rws: 524288,
+                    max_copy_rows: 524288,
                     max_exp_steps: 27900,
                     min_k: 21,
                     pad_to: 0,
-                    min_k_aggregation: 24,
+                    min_k_aggregation: 26,
                     keccak_padding: 500000,
                 };
                 $on_match
